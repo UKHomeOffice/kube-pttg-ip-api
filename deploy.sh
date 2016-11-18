1a8f82287f7db64c73b42e0301f6def572c7b80d
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-18 15:24:18 +0000

    Correct environment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export KUBE_NAMESPACE=${KUBE_NAMESPACE:-${DRONE_DEPLOY_TO}}
-export ENVIRONMENT=${ENVIRONMENT:-dev}
+export ENVIRONMENT=${ENVIRONMENT:dev}
 export APP=${APP:pttg-ip-api}
 export KUBE_SERVER=${KUBE_SERVER_DEV}
 
