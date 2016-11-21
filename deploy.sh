4c36c221c5f96f232aa0d4cb14ddc95a4afe12dc
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-21 10:21:40 +0000

    Debugging

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ export APP=pttg-ip-api
 export KUBE_SERVER=${KUBE_SERVER_DEV}
 
 cd kd
+pwd
+ls
 kd --insecure-skip-tls-verify \
    --file dev/pttg-ip-api-deployment.yaml \
    --file dev/pttg-ip-api-svc.yaml
