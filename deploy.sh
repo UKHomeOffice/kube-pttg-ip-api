bb42932323b8d450ec5b03e4326f67ff9008dad2
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-21 11:46:28 +0000

    Debugging

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ export ENVIRONMENT=${ENVIRONMENT}
 export APP=pttg-ip-api
 export KUBE_SERVER=${KUBE_SERVER_DEV}
 
-echo hello
+echo "deploying ..."
 cd kd
 pwd
 ls
 kd --insecure-skip-tls-verify --file dev/pttg-ip-api-deployment.yaml --file dev/pttg-ip-api-svc.yaml
+echo "deployment finished"
