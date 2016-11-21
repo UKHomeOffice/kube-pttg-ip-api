8052b56f35e4c33c698fe11b4102df57fc70a0d3
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-21 14:14:30 +0000

    Increase retries

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
-kd --retries=20 \
-   --insecure-skip-tls-verify \
+kd --insecure-skip-tls-verify --retries=20 \
    --file ${ENVIRONMENT}/pttg-ip-api-deployment.yaml \
    --file ${ENVIRONMENT}/pttg-ip-api-svc.yaml
