1870bbee42cdc26ad0aca533744ef894a50c2d8e
Alistair Todd <alistair.todd@digital.homeoffice.gov.uk>
2016-11-21 10:04:12 +0000

    Remove useless debug flag

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ export APP=pttg-ip-api
 export KUBE_SERVER=${KUBE_SERVER_DEV}
 
 cd kd
-kd --debug --insecure-skip-tls-verify \
+kd --insecure-skip-tls-verify \
    --file dev/pttg-ip-api-deployment.yaml \
    --file dev/pttg-ip-api-svc.yaml
