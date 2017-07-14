2535e35f64585c4934222fe8983f1879a9bf94dd
Andy Marke <andy.marke@gmail.com>
2017-07-14 15:41:56 +0100

    Increase memory and created single config for all environments

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export ENVIRONMENT=${ENVIRONMENT}
-export APP=pttg-ip-api
 export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
 kd --insecure-skip-tls-verify --retries=20 \
-   --file ${ENVIRONMENT}/pttg-ip-api-deployment.yaml \
-   --file ${ENVIRONMENT}/pttg-ip-api-svc.yaml
+   --file deployment.yaml \
+   --file service.yaml
