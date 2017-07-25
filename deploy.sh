9856721855765f1c1eef83aaecc5531a35c554c7
Andy Marke <andy.marke@gmail.com>
2017-07-25 13:31:56 +0100

    use KUBE_SERVER, removing reference to DEV

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export ENVIRONMENT=${ENVIRONMENT}
-export KUBE_SERVER=${KUBE_SERVER_DEV}
+export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
 kd --insecure-skip-tls-verify --retries=20 \
-   --file deployment.yaml \
-   --file service.yaml
+   --file service.yaml \
+   --file deployment.yaml
