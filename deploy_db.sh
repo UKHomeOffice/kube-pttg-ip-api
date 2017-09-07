3823918a5c54c444c4f130e6bc4a9045a2d24d60
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2017-09-07 14:46:28 +0100

    The config for creating the Kubernetes pttg-ip-api-db pod is now in its own git repo - kube-pttg-postgres

--- deploy_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-export KUBE_NAMESPACE=pt-i-dev
-export ENVIRONMENT=dev
-export KUBE_SERVER=https://kube-dev.dsp.notprod.homeoffice.gov.uk
-export KUBE_TOKEN=9f7b18cc-0e18-11e7-98cf-67e86f87fd2d
-
-cd kd
-kd --insecure-skip-tls-verify \
-   --file deployment_db.yaml
