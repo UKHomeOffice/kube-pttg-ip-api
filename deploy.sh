7a17f54fb5f81ce9d6c6bc87e46c79ce0488c941
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-08-16 11:07:15 +0100

    Revert "EE-12559 The downscaler from time is respected after deployments"
    This reverts commit a04140fc

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ fi
 if [[ ${ENVIRONMENT} == "pr" ]] ; then
     echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
     export KUBE_TOKEN=${PTTG_IP_PR}
+    # An empty "downscaler/uptime" annotation is ignored
     export UPTIME_SCHEDULE=''
     export ARCHIVE_CRON_SCHEDULE='30 7 * * *'
 else
@@ -22,7 +23,8 @@ else
         export KUBE_TOKEN=${PTTG_IP_DEV}
     fi
     # Scale down all pods every night in non-prod.
-    export UPTIME_SCHEDULE='Mon-Sun 07:00-20:00 Europe/London'
+    # Using the same from/to time on annotation "downscaler/uptime" scales down at that time but never scales up.
+    export UPTIME_SCHEDULE='Mon-Sun 20:00-20:00 Europe/London'
     # Never run the archive in non-prod.  We don't know if the pods are actually up, and if they are we may as well save the data to test archiving anyway.
 #    export ARCHIVE_CRON_SCHEDULE='* * * * 3000'
     export ARCHIVE_CRON_SCHEDULE='30 7 * * *'
