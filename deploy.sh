a04140fc899cfb5b368321dd632c86f6c24eab12
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-08-16 10:57:29 +0100

    EE-12559 The downscaler from time is respected after deployments

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,6 @@ fi
 if [[ ${ENVIRONMENT} == "pr" ]] ; then
     echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
     export KUBE_TOKEN=${PTTG_IP_PR}
-    # An empty "downscaler/uptime" annotation is ignored
     export UPTIME_SCHEDULE=''
     export ARCHIVE_CRON_SCHEDULE='30 7 * * *'
 else
@@ -23,8 +22,7 @@ else
         export KUBE_TOKEN=${PTTG_IP_DEV}
     fi
     # Scale down all pods every night in non-prod.
-    # Using the same from/to time on annotation "downscaler/uptime" scales down at that time but never scales up.
-    export UPTIME_SCHEDULE='Mon-Sun 20:00-20:00 Europe/London'
+    export UPTIME_SCHEDULE='Mon-Sun 07:00-20:00 Europe/London'
     # Never run the archive in non-prod.  We don't know if the pods are actually up, and if they are we may as well save the data to test archiving anyway.
 #    export ARCHIVE_CRON_SCHEDULE='* * * * 3000'
     export ARCHIVE_CRON_SCHEDULE='30 7 * * *'
