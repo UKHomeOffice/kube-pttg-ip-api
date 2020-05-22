cbd58f862f15058f8c681878d200dbeb8371dadc
Tim-Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2020-05-22 15:53:10 +0100

    EE-32216: Use ACP platform-ca generated server cert and CA for truststore.

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
+export DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-pttg-ip-api}
 
 if [[ -z ${VERSION} ]] ; then
     export VERSION=${IMAGE_VERSION}
 fi
 
-echo "deploy ${VERSION} to ${ENVIRONMENT} namespace - using Kube token stored as drone secret"
+echo "deploy version: ${VERSION} of: ${DEPLOYMENT_NAME} to:${ENVIRONMENT} namespace - using Kube token stored as drone secret"
 
 if [[ ${ENVIRONMENT} == "pr" ]] ; then
     export KUBE_TOKEN=${PTTG_IP_PR}
@@ -15,9 +16,10 @@ else
     export KUBE_TOKEN=${PTTG_IP_DEV}
 fi
 
-cd kd
+cd kd || exit
 
 kd --insecure-skip-tls-verify \
+    -f ingress-certificate.yaml \
     -f audit-archive-cronjob.yaml \
     -f networkPolicy.yaml \
     -f deployment.yaml \
